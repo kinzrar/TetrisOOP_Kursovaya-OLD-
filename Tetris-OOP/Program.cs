d4dd62a2fbc327562a45f51409d1f981ba0505ec
Дима Чернявский <dimasikok-2003@mail.ru>
2022-09-08 00:44:22 +0300

    Добавьте файлы проекта.

---
+++ Tetris-OOP/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace Tetris
+{
+	internal static class Program
+	{
+		/// <summary>Главная точка входа для приложения.</summary>
+		[STAThread]
+		private static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+	}
+}
